efe0432d483c4fdf6172259c9dbdca062e720dbc
Ittipon <bay_xyz@hotmail.com>
2014-05-16 13:42:23 +0700

    Add usage example

---
+++ unity3d-project/Assets/Scripts/example.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using genc.genpon;
+using Prime31;
+
+public class Main : MonoBehaviour {
+
+	public DailyNotifyObject dailyNotifyObject;
+	void Awake () {
+		if (dailyNotifyObject == null) {
+			dailyNotifyObject = new GameObject ("_testObj").AddComponent<DailyNotifyObject> ();
+		}
+		dailyNotifyObject.Init ();
+		dailyNotifyObject.SetNotifyAppearance ("Title (android only)", "Content as you wish");
+	}
+}
